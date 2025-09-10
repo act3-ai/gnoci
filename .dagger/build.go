95b711a1e19334b0122a30a342871be1051feeb7
nathan-joslin <nathan.joslin@udri.udayton.edu>
2025-09-10 18:38:22 -0400

    add multi-platform builds with dagger

--- .dagger/build.go
+++ .dagger/build.go
@@ -4,7 +4,15 @@ import (
 	"context"
 	"dagger/gnoci/internal/dagger"
 	"fmt"
+	"path"
+	"strings"
+	"sync"
+
+	"github.com/sourcegraph/conc/pool"
 )
+
+// executable name
+const execName = "git-remote-oci"
 
 // Build an executable.
 func (g *Gnoci) Build(ctx context.Context,
@@ -19,7 +27,7 @@ func (g *Gnoci) Build(ctx context.Context,
 	// trim source for better caching
 	src := g.Source.Filter(dagger.DirectoryFilterOpts{
 		Exclude: []string{"**/*_test.go"},
-		Include: []string{"**/*.go"},
+		Include: []string{"**/*.go", "go.mod", "go.sum"},
 	})
 
 	ldflags := []string{"-s", "-w", `-extldflags "-static"`}
@@ -28,12 +36,50 @@ func (g *Gnoci) Build(ctx context.Context,
 	}
 	return g.goWithSource(src).
 		Build(dagger.GoWithSourceBuildOpts{
-			Pkg:      "./cmd/git-remote-oci",
+			Pkg:      fmt.Sprintf("./cmd/%s", execName),
 			Platform: platform,
 			Trimpath: true,
 			Ldflags:  ldflags,
 		}).
-		WithName("git-remote-oci")
+		WithName(execName)
+}
+
+// Build binaries for multiple platforms, nested in directories
+// named by platform.
+//
+//nolint:staticcheck
+func (g *Gnoci) BuildPlatforms(ctx context.Context,
+	// Build version
+	// +optional
+	version string,
+	// build platforms
+	// +default=["linux/amd64","linux/arm64","darwin/arm64"]
+	platforms []dagger.Platform,
+) *dagger.Directory {
+	var mux sync.Mutex
+	builds := dag.Directory()
+
+	p := pool.New().WithContext(ctx)
+
+	for _, platform := range platforms {
+		p.Go(func(ctx context.Context) error {
+			ctx, span := Tracer().Start(ctx, fmt.Sprintf("Build %s", platform))
+			defer span.End()
+
+			bin := g.Build(ctx, version, platform)
+
+			mux.Lock()
+			defer mux.Unlock()
+			builds = builds.WithFile(
+				path.Join(strings.ReplaceAll(string(platform), "/", "-"), execName),
+				bin)
+
+			return nil
+		})
+	}
+	_ = p.Wait() // throw away err, as we can't get one
+
+	return builds
 }
 
 // Initializes a container with Go and the source.
