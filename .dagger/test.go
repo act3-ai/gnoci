57e42d6a6ddde3d92731d25ec6b35be15430eb84
nathan-joslin <nathan.joslin@udri.udayton.edu>
2025-11-03 19:00:01 -0500

    add dagger testing container and start basic test helper

--- .dagger/test.go
+++ .dagger/test.go
@@ -3,6 +3,10 @@ package main
 import (
 	"context"
 	"dagger/gnoci/internal/dagger"
+	"fmt"
+	"path/filepath"
+
+	"github.com/sourcegraph/conc/pool"
 )
 
 // Run tests.
@@ -66,3 +70,89 @@ func (t *Test) CoverageTreeMap(ctx context.Context,
 
 	return dag.File(coverageTreemapFile, svg)
 }
+
+func (t *Test) PushFetch(ctx context.Context,
+	// Git reference to test repository
+	gitRef *dagger.GitRef,
+) (string, error) {
+	const srcDir = "test-src"
+	src := gitRef.Tree()
+
+	// get container with git-remote-oci and git-lfs-remote-oci
+	ctr, err := t.containerWithHelpers(ctx)
+	if err != nil {
+		return "", fmt.Errorf("creating test container: %w", err)
+	}
+
+	// add test source
+	ctr = ctr.WithDirectory(srcDir, src).
+		WithWorkdir(srcDir)
+
+	// configure git
+
+	// configure git-lfs
+
+	// connect to registry
+
+	// push
+
+	// pull (in second dir or another container?)
+
+	// get metadata
+
+	// return metadata on stdout
+
+	return "", fmt.Errorf("not implemented")
+}
+
+// containerWithHelpers creates a container with git-remote-oci and git-lfs-remote-oci.
+func (t *Test) containerWithHelpers(ctx context.Context) (*dagger.Container, error) {
+	platform := dagger.Platform("linux/amd64")
+
+	var gitHelper, gitLFSHelper *dagger.File
+	var gitHelperName, gitLFSHelperName string
+	p := pool.New().WithContext(ctx)
+	p.Go(func(ctx context.Context) error {
+		gitHelper = t.BuildGit(ctx, "test-dev", platform)
+
+		var err error
+		gitHelperName, err = gitHelper.Name(ctx)
+		if err != nil {
+			return fmt.Errorf("determining name of git helper exec: %w", err)
+		}
+		return nil
+	})
+
+	p.Go(func(ctx context.Context) error {
+		gitLFSHelper = t.BuildGitLFS(ctx, "test-dev", platform)
+
+		var err error
+		gitLFSHelperName, err = gitLFSHelper.Name(ctx)
+		if err != nil {
+			return fmt.Errorf("determining name of git-lfs helper exec: %w", err)
+		}
+		return nil
+	})
+
+	_ = p.Wait() // throw away err, as we can't get one
+
+	return dag.Alpine(dagger.AlpineOpts{
+		Packages: []string{"git", "git-lfs"},
+	}).
+		Container().
+		WithFile(filepath.Join("usr", "local", "bin", gitHelperName), gitHelper).
+		WithFile(filepath.Join("usr", "local", "bin", gitLFSHelperName), gitLFSHelper), nil
+}
+
+// func (t *Test) TestCtr(ctx context.Context) (*dagger.Container, error) {
+// 	ctr, err := t.containerWithHelpers(ctx)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("failed to create container: %w", err)
+// 	}
+
+// 	ctr = ctr.Terminal()
+
+// 	ctr.WithExec([]string{"git", "--version"})
+
+// 	return ctr, nil
+// }
