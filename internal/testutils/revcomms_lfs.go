06b9a033b91eee6d8fbe39917644265acab7ef73
nathan-joslin <nathan.joslin@udri.udayton.edu>
2025-11-28 16:20:38 -0500

    add testutil ReverseCommunicatorLFS
    - The reverse of lfs/comms.Communicator, acting as git-lfs sending requests

---
+++ internal/testutils/revcomms_lfs.go
@@ -0,0 +1,236 @@
+package testutils
+
+import (
+	"bufio"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+
+	"github.com/act3-ai/gnoci/pkg/protocol/lfs"
+)
+
+// ReverseCommunicatorLFS is the reverse of lfs [comms.Communicator], acting as git-lfs
+// sending custom transfer protocol requests and receiving responses.
+type ReverseCommunicatorLFS interface {
+	// SendInitRequest sends an [lfs.InitRequest].
+	SendInitRequest(op lfs.Operation, remote string) error
+	// ReceiveInitResponse receives an [lfs.InitResponse].
+	ReceiveInitResponse() error
+	// SendTransferUploadRequest sends an [lfs.TransferRequest] with event
+	// [lfs.UploadEvent].
+	SendTransferUploadRequest(oid string, size int64, path string) error
+	// SendTransferDownloadRequest sends an [lfs.TransferRequest] with event
+	// [lfs.DownloadEvent].
+	SendTransferDownloadRequest(oid string, size int64) error
+	// ReceiveProgressResponse receives an [lfs.ProgressResponse].
+	ReceiveProgressResponse() error
+	// ReceiveTransferResponse receives an [lfs.TransferResponse].
+	ReceiveTransferResponse() error
+	// SendTerminateRequest sends an [lfs.TransferRequest] with event
+	// [lfs.TerminateEvent].
+	SendTerminateRequest() error
+}
+
+// NewReverseCommunicatorLFS initializes a [ReverseCommunicatorLFS].
+func NewReverseCommunicatorLFS(in io.Reader, out io.Writer) ReverseCommunicatorLFS {
+	return &reverseCommunicatorLFS{
+		in:  bufio.NewScanner(in),
+		out: out,
+	}
+}
+
+// reverseCommunicatorLFS implements [ReverseCommunicatorLFS].
+type reverseCommunicatorLFS struct {
+	in  *bufio.Scanner
+	out io.Writer
+}
+
+func (g *reverseCommunicatorLFS) SendInitRequest(op lfs.Operation, remote string) error {
+	// TODO: we don't support concurrency settings, but if we ever do add plumbing here
+	req := &lfs.InitRequest{
+		Event:               lfs.InitEvent,
+		Operation:           op,
+		Remote:              remote,
+		Concurrent:          false,
+		ConcurrentTransfers: 1,
+	}
+
+	// validate op and remote
+	err := req.Validate()
+	if err != nil {
+		return fmt.Errorf("invalid InitRequest: %w", err)
+	}
+
+	reqRaw, err := json.Marshal(req)
+	if err != nil {
+		return fmt.Errorf("encoding InitRequest: %w", err)
+	}
+
+	_, err = g.out.Write(withNewline(reqRaw))
+	if err != nil {
+		return fmt.Errorf("writing InitRequest: %w", err)
+	}
+
+	return nil
+}
+
+func (g *reverseCommunicatorLFS) ReceiveInitResponse() error {
+
+	line, err := g.readLine()
+	if err != nil {
+		return err
+	}
+
+	var initResp *lfs.InitResponse
+	if err := json.Unmarshal(line, &initResp); err != nil {
+		return fmt.Errorf("decoding InitRequest: %w", err)
+	}
+
+	if initResp.Error.Message != "" {
+		return fmt.Errorf("received InitResponse error: code = %d, message = %s", initResp.Error.Code, initResp.Error.Message)
+	}
+
+	return nil
+}
+
+func (g *reverseCommunicatorLFS) SendTransferUploadRequest(oid string, size int64, path string) error {
+	if oid == "" {
+		return fmt.Errorf("empty oid")
+	}
+	if size < 1 {
+		return fmt.Errorf("invalid oid size %d, expected > 0", size)
+	}
+	if path == "" {
+		return fmt.Errorf("empty path")
+	}
+
+	req := &lfs.TransferRequest{
+		Event: lfs.UploadEvent,
+		Oid:   oid,
+		Size:  size,
+		Path:  path,
+	}
+
+	reqRaw, err := json.Marshal(req)
+	if err != nil {
+		return fmt.Errorf("encoding TransferRequest: %w", err)
+	}
+
+	_, err = g.out.Write(withNewline(reqRaw))
+	if err != nil {
+		return fmt.Errorf("writing TransferRequest: %w", err)
+	}
+
+	return nil
+}
+
+func (g *reverseCommunicatorLFS) SendTransferDownloadRequest(oid string, size int64) error {
+	if oid == "" {
+		return fmt.Errorf("empty oid")
+	}
+	if size < 1 {
+		return fmt.Errorf("invalid oid size %d, expected > 0", size)
+	}
+
+	req := &lfs.TransferRequest{
+		Event: lfs.DownloadEvent,
+		Oid:   oid,
+		Size:  size,
+	}
+
+	reqRaw, err := json.Marshal(req)
+	if err != nil {
+		return fmt.Errorf("encoding TransferRequest: %w", err)
+	}
+
+	_, err = g.out.Write(withNewline(reqRaw))
+	if err != nil {
+		return fmt.Errorf("writing TransferRequest: %w", err)
+	}
+
+	return nil
+}
+
+func (g *reverseCommunicatorLFS) ReceiveProgressResponse() error {
+	line, err := g.readLine()
+	if err != nil {
+		return err
+	}
+
+	var progressResp *lfs.ProgressResponse
+	if err := json.Unmarshal(line, &progressResp); err != nil {
+		return fmt.Errorf("decoding TransferResponse: %w", err)
+	}
+
+	msgErrs := make([]error, 0)
+	if progressResp.Event != lfs.ProgessEvent {
+		msgErrs = append(msgErrs, fmt.Errorf("invalid progress event type %s, expected %s", progressResp.Event, lfs.ProgessEvent))
+	}
+	if progressResp.Oid == "" {
+		msgErrs = append(msgErrs, fmt.Errorf("received empty progress oid"))
+	}
+
+	return errors.Join(msgErrs...)
+}
+
+func (g *reverseCommunicatorLFS) ReceiveTransferResponse() error {
+	line, err := g.readLine()
+	if err != nil {
+		return err
+	}
+
+	var transferResp *lfs.TransferResponse
+	if err := json.Unmarshal(line, &transferResp); err != nil {
+		return fmt.Errorf("decoding TransferResponse: %w", err)
+	}
+
+	if transferResp.Path == "" {
+		return fmt.Errorf("no path provided in TransferResponse")
+	}
+
+	if transferResp.Oid == "" {
+		return fmt.Errorf("no oid provided in TransferResponse")
+	}
+
+	if transferResp.Error != nil && transferResp.Error.Message != "" {
+		return fmt.Errorf("received TransferResponse error: code = %d, message = %s", transferResp.Error.Code, transferResp.Error.Message)
+	}
+
+	return nil
+}
+
+func (g *reverseCommunicatorLFS) SendTerminateRequest() error {
+	req := &lfs.TransferRequest{
+		Event: lfs.TerminateEvent,
+	}
+
+	reqRaw, err := json.Marshal(req)
+	if err != nil {
+		return fmt.Errorf("encoding terminate TransferRequest: %w", err)
+	}
+
+	_, err = g.out.Write(withNewline(reqRaw))
+	if err != nil {
+		return fmt.Errorf("writing terminate TransferRequest: %w", err)
+	}
+
+	return nil
+}
+
+func (g *reverseCommunicatorLFS) readLine() ([]byte, error) {
+	ok := g.in.Scan()
+	switch {
+	case !ok && g.in.Err() != nil:
+		return nil, fmt.Errorf("reading single command from git-lfs-remote-oci: %w", g.in.Err())
+	case !ok:
+		// EOF
+		return nil, nil
+	default:
+		return g.in.Bytes(), nil
+	}
+}
+
+func withNewline(line []byte) []byte {
+	return append(line, []byte("\n")...) // TODO: can we use a rune?
+}
---
+++ internal/testutils/revcomms_lfs_test.go
@@ -0,0 +1,462 @@
+package testutils
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io"
+	"strings"
+	"testing"
+
+	"github.com/act3-ai/gnoci/pkg/protocol/lfs"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewReverseCommunicatorLFS(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		in := strings.NewReader("foo")
+		out := new(bytes.Buffer)
+
+		revCommI := NewReverseCommunicatorLFS(in, out)
+		assert.NotNil(t, revCommI)
+
+		revComm, ok := revCommI.(*reverseCommunicatorLFS)
+		assert.True(t, ok)
+		assert.NotNil(t, revComm)
+		assert.NotNil(t, revComm.in)
+		assert.Equal(t, out, revComm.out)
+	})
+}
+
+func Test_reverseCommunicatorLFS_SendInitRequest(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		out := new(bytes.Buffer)
+
+		revcomm := NewReverseCommunicatorLFS(nil, out)
+
+		err := revcomm.SendInitRequest(lfs.UploadOperation, "foo/bar/foofoo/barbar.foo")
+		assert.NoError(t, err)
+
+		outRaw, err := io.ReadAll(out)
+		assert.NoError(t, err)
+
+		var req lfs.InitRequest
+		err = json.Unmarshal(outRaw, &req)
+		assert.NoError(t, err)
+
+		err = req.Validate()
+		assert.NoError(t, err)
+	})
+
+	t.Run("Invalid Request", func(t *testing.T) {
+		out := new(bytes.Buffer)
+
+		revcomm := NewReverseCommunicatorLFS(nil, out)
+
+		err := revcomm.SendInitRequest(lfs.Operation("foo"), "foo/bar/foofoo/barbar.foo")
+		assert.Error(t, err)
+
+	})
+}
+
+func Test_reverseCommunicatorLFS_ReceiveInitResponse(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		// this test intentionally does not use a comms.Communicator, incase
+		// bugs are introduced.
+		in := new(bytes.Buffer)
+		revcomm := NewReverseCommunicatorLFS(in, nil)
+
+		raw := []byte("{}")
+		_, err := in.Write(withNewline(raw))
+		assert.NoError(t, err)
+
+		err = revcomm.ReceiveInitResponse()
+		assert.NoError(t, err)
+	})
+
+	t.Run("Error", func(t *testing.T) {
+		// this test intentionally does not use a comms.Communicator, incase
+		// bugs are introduced.
+		in := new(bytes.Buffer)
+		revcomm := NewReverseCommunicatorLFS(in, nil)
+
+		initResp := lfs.InitResponse{
+			Error: lfs.ErrCodeMessage{
+				Code:    1,
+				Message: errors.New("init error").Error(),
+			},
+		}
+
+		raw, err := json.Marshal(initResp)
+		assert.NoError(t, err)
+
+		_, err = in.Write(withNewline(raw))
+		assert.NoError(t, err)
+
+		err = revcomm.ReceiveInitResponse()
+		assert.Error(t, err)
+	})
+
+	t.Run("Invalid JSON", func(t *testing.T) {
+		// this test intentionally does not use a comms.Communicator, incase
+		// bugs are introduced.
+		in := new(bytes.Buffer)
+		revcomm := NewReverseCommunicatorLFS(in, nil)
+
+		raw := []byte("invalid json")
+		_, err := in.Write(withNewline(raw))
+		assert.NoError(t, err)
+
+		err = revcomm.ReceiveInitResponse()
+		assert.Error(t, err)
+	})
+}
+
+func Test_reverseCommunicatorLFS_SendTransferUploadRequest(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		out := new(bytes.Buffer)
+
+		revcomm := NewReverseCommunicatorLFS(nil, out)
+
+		const (
+			oid  = "123456789"
+			size = int64(10)
+			path = "path/foo"
+		)
+
+		err := revcomm.SendTransferUploadRequest(oid, size, path)
+		assert.NoError(t, err)
+
+		outRaw, err := io.ReadAll(out)
+		assert.NoError(t, err)
+
+		var req lfs.TransferRequest
+		err = json.Unmarshal(outRaw, &req)
+		assert.NoError(t, err)
+
+		assert.Equal(t, lfs.UploadEvent, req.Event)
+		assert.Equal(t, oid, req.Oid)
+		assert.Equal(t, size, req.Size)
+		assert.Equal(t, path, req.Path)
+	})
+
+	t.Run("Mission Oid", func(t *testing.T) {
+		out := new(bytes.Buffer)
+
+		revcomm := NewReverseCommunicatorLFS(nil, out)
+
+		const (
+			oid  = ""
+			size = int64(10)
+			path = "path/foo"
+		)
+
+		err := revcomm.SendTransferUploadRequest(oid, size, path)
+		assert.Error(t, err)
+	})
+
+	t.Run("Invalid Size", func(t *testing.T) {
+		out := new(bytes.Buffer)
+
+		revcomm := NewReverseCommunicatorLFS(nil, out)
+
+		const (
+			oid  = "123456789"
+			size = int64(0)
+			path = "path/foo"
+		)
+
+		err := revcomm.SendTransferUploadRequest(oid, size, path)
+		assert.Error(t, err)
+	})
+
+	t.Run("Missing Path", func(t *testing.T) {
+		out := new(bytes.Buffer)
+
+		revcomm := NewReverseCommunicatorLFS(nil, out)
+
+		const (
+			oid  = "123456789"
+			size = int64(10)
+			path = ""
+		)
+
+		err := revcomm.SendTransferUploadRequest(oid, size, path)
+		assert.Error(t, err)
+	})
+}
+
+func Test_reverseCommunicatorLFS_SendTransferDownloadRequest(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		out := new(bytes.Buffer)
+
+		revcomm := NewReverseCommunicatorLFS(nil, out)
+
+		const (
+			oid  = "123456789"
+			size = int64(10)
+		)
+
+		err := revcomm.SendTransferDownloadRequest(oid, size)
+		assert.NoError(t, err)
+
+		outRaw, err := io.ReadAll(out)
+		assert.NoError(t, err)
+
+		var req lfs.TransferRequest
+		err = json.Unmarshal(outRaw, &req)
+		assert.NoError(t, err)
+
+		assert.Equal(t, lfs.DownloadEvent, req.Event)
+		assert.Equal(t, oid, req.Oid)
+		assert.Equal(t, size, req.Size)
+	})
+
+	t.Run("Missing Oid", func(t *testing.T) {
+		out := new(bytes.Buffer)
+
+		revcomm := NewReverseCommunicatorLFS(nil, out)
+
+		const (
+			oid  = ""
+			size = int64(10)
+		)
+
+		err := revcomm.SendTransferDownloadRequest(oid, size)
+		assert.Error(t, err)
+	})
+
+	t.Run("Invalid Size", func(t *testing.T) {
+		out := new(bytes.Buffer)
+
+		revcomm := NewReverseCommunicatorLFS(nil, out)
+
+		const (
+			oid  = "123456789"
+			size = int64(-1)
+		)
+
+		err := revcomm.SendTransferDownloadRequest(oid, size)
+		assert.Error(t, err)
+	})
+}
+
+func Test_reverseCommunicatorLFS_ReceiveProgressResponse(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		// this test intentionally does not use a comms.Communicator, incase
+		// bugs are introduced.
+		in := new(bytes.Buffer)
+		revcomm := NewReverseCommunicatorLFS(in, nil)
+
+		req := &lfs.ProgressResponse{
+			Event:          lfs.ProgessEvent,
+			Oid:            "123456789",
+			BytesSoFar:     10,
+			BytesSinceLast: 5,
+		}
+
+		raw, err := json.Marshal(req)
+		assert.NoError(t, err)
+
+		_, err = in.Write(withNewline(raw))
+		assert.NoError(t, err)
+
+		err = revcomm.ReceiveProgressResponse()
+		assert.NoError(t, err)
+	})
+
+	t.Run("Invalid Event", func(t *testing.T) {
+		// this test intentionally does not use a comms.Communicator, incase
+		// bugs are introduced.
+		in := new(bytes.Buffer)
+		revcomm := NewReverseCommunicatorLFS(in, nil)
+
+		req := &lfs.ProgressResponse{
+			Event:          lfs.Event("foo"),
+			Oid:            "123456789",
+			BytesSoFar:     10,
+			BytesSinceLast: 5,
+		}
+
+		raw, err := json.Marshal(req)
+		assert.NoError(t, err)
+
+		_, err = in.Write(withNewline(raw))
+		assert.NoError(t, err)
+
+		err = revcomm.ReceiveProgressResponse()
+		assert.Error(t, err)
+	})
+
+	t.Run("Missing Oid", func(t *testing.T) {
+		// this test intentionally does not use a comms.Communicator, incase
+		// bugs are introduced.
+		in := new(bytes.Buffer)
+		revcomm := NewReverseCommunicatorLFS(in, nil)
+
+		req := &lfs.ProgressResponse{
+			Event:          lfs.ProgessEvent,
+			Oid:            "",
+			BytesSoFar:     10,
+			BytesSinceLast: 5,
+		}
+
+		raw, err := json.Marshal(req)
+		assert.NoError(t, err)
+
+		_, err = in.Write(withNewline(raw))
+		assert.NoError(t, err)
+
+		err = revcomm.ReceiveProgressResponse()
+		assert.Error(t, err)
+	})
+}
+
+func Test_reverseCommunicatorLFS_ReceiveTransferResponse(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		// this test intentionally does not use a comms.Communicator, incase
+		// bugs are introduced.
+		in := new(bytes.Buffer)
+		revcomm := NewReverseCommunicatorLFS(in, nil)
+
+		req := &lfs.TransferResponse{
+			Event: lfs.CompleteEvent,
+			Oid:   "123456789",
+			Path:  "path/foo",
+		}
+
+		raw, err := json.Marshal(req)
+		assert.NoError(t, err)
+
+		_, err = in.Write(withNewline(raw))
+		assert.NoError(t, err)
+
+		err = revcomm.ReceiveTransferResponse()
+		assert.NoError(t, err)
+	})
+
+	t.Run("Missing Path", func(t *testing.T) {
+		// this test intentionally does not use a comms.Communicator, incase
+		// bugs are introduced.
+		in := new(bytes.Buffer)
+		revcomm := NewReverseCommunicatorLFS(in, nil)
+
+		req := &lfs.TransferResponse{
+			Event: lfs.CompleteEvent,
+			Oid:   "123456789",
+			Path:  "",
+		}
+
+		raw, err := json.Marshal(req)
+		assert.NoError(t, err)
+
+		_, err = in.Write(withNewline(raw))
+		assert.NoError(t, err)
+
+		err = revcomm.ReceiveTransferResponse()
+		assert.Error(t, err)
+	})
+
+	t.Run("Missing Oid", func(t *testing.T) {
+		// this test intentionally does not use a comms.Communicator, incase
+		// bugs are introduced.
+		in := new(bytes.Buffer)
+		revcomm := NewReverseCommunicatorLFS(in, nil)
+
+		req := &lfs.TransferResponse{
+			Event: lfs.CompleteEvent,
+			Oid:   "",
+			Path:  "path/foo",
+		}
+
+		raw, err := json.Marshal(req)
+		assert.NoError(t, err)
+
+		_, err = in.Write(withNewline(raw))
+		assert.NoError(t, err)
+
+		err = revcomm.ReceiveTransferResponse()
+		assert.Error(t, err)
+	})
+
+	t.Run("Error", func(t *testing.T) {
+		// this test intentionally does not use a comms.Communicator, incase
+		// bugs are introduced.
+		in := new(bytes.Buffer)
+		revcomm := NewReverseCommunicatorLFS(in, nil)
+
+		req := &lfs.TransferResponse{
+			Event: lfs.CompleteEvent,
+			Oid:   "123456789",
+			Path:  "path/foo",
+			Error: &lfs.ErrCodeMessage{
+				Code:    1,
+				Message: errors.New("transfer error").Error(),
+			},
+		}
+
+		raw, err := json.Marshal(req)
+		assert.NoError(t, err)
+
+		_, err = in.Write(withNewline(raw))
+		assert.NoError(t, err)
+
+		err = revcomm.ReceiveTransferResponse()
+		assert.Error(t, err)
+	})
+}
+
+func Test_reverseCommunicatorLFS_SendTerminateRequest(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		out := new(bytes.Buffer)
+
+		revcomm := NewReverseCommunicatorLFS(nil, out)
+
+		err := revcomm.SendTerminateRequest()
+		assert.NoError(t, err)
+
+		outRaw, err := io.ReadAll(out)
+		assert.NoError(t, err)
+
+		var req lfs.TransferRequest
+		err = json.Unmarshal(outRaw, &req)
+		assert.NoError(t, err)
+
+		assert.Equal(t, lfs.TerminateEvent, req.Event)
+	})
+}
+
+func Test_reverseCommunicatorLFS_readLine(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		in := new(bytes.Buffer)
+
+		revcomm := &reverseCommunicatorLFS{
+			in:  bufio.NewScanner(in),
+			out: nil,
+		}
+
+		_, err := in.WriteString("foo\n")
+		assert.NoError(t, err)
+
+		outRaw, err := revcomm.readLine()
+		assert.NoError(t, err)
+		assert.Equal(t, string(outRaw), "foo")
+	})
+
+	t.Run("EOF", func(t *testing.T) {
+		in := new(bytes.Buffer)
+
+		revcomm := &reverseCommunicatorLFS{
+			in:  bufio.NewScanner(in),
+			out: nil,
+		}
+
+		_, err := in.WriteString("")
+		assert.NoError(t, err)
+
+		outRaw, err := revcomm.readLine()
+		assert.NoError(t, err)
+		assert.Nil(t, outRaw)
+	})
+}
